fb5b03d4953e412ad269bf5587cde71d2feb6051
sara <sara.souza.9@hotmail.com>
2020-04-15 23:56:38 -0300

    Updates

---
+++ precache-manifest.a7c08b0f1cc1583694f5c03b4e1c769b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c2701cbac21428342aa3d4a8418db9",
+    "url": "/PokedexFrontend/index.html"
+  },
+  {
+    "revision": "83a35af8ba9c59adab59",
+    "url": "/PokedexFrontend/static/css/main.65a84577.chunk.css"
+  },
+  {
+    "revision": "9857795051b26b688881",
+    "url": "/PokedexFrontend/static/js/2.b08e80f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PokedexFrontend/static/js/2.b08e80f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a35af8ba9c59adab59",
+    "url": "/PokedexFrontend/static/js/main.38f4ffd7.chunk.js"
+  },
+  {
+    "revision": "e5bda5e6c91e10825c20",
+    "url": "/PokedexFrontend/static/js/runtime-main.dc268192.js"
+  }
+]);
